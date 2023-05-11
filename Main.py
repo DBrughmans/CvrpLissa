f7efb7791c002c8e9af6cd52be5f26c6c13e49ad
brandon8578 <dieterbrughmans@gmail.com>
2023-05-11 11:47:57 +0200

    global residual normality added

--- Main.py
+++ Main.py
@@ -19,17 +19,19 @@ PATHS = {
     'ILS-RVND-SP_Golden':'./Data/ILS-RVND-SP_Golden.csv'
 }
 
-def lmem_normality(name):
+def lmem_normality_grouped(name):
     data = pd.read_csv(PATHS[name])
+    methods = list(data.columns)[::-1][:-1]
     data = pd.melt(data, id_vars=["Instance"], var_name="Method", value_name="Value")
 
+    data['Method'] = pd.Categorical(data['Method'], categories=methods, ordered=False)
     # Run LMER
     model = smf.mixedlm("Value ~ Method", data=data, groups=data["Instance"])
     result = model.fit()
 
     log_data = copy.deepcopy(data)
     log_data['Value'] = np.log(log_data['Value'])
-    log_model = smf.mixedlm("Value ~ Method", data=log_data, groups=data["Instance"])
+    log_model = smf.mixedlm(f"Value ~ Method", data=log_data, groups=data["Instance"])
     log_result = log_model.fit()
     # Obtain the residuals
     residuals = result.resid
@@ -48,5 +50,26 @@ def lmem_normality(name):
 
     df_results.to_csv(f'./Data/Results/{name}.csv')
 
+def lmem_normality(name):
+    data = pd.read_csv(PATHS[name])
+    methods = list(data.columns)[::-1][:-1]
+    data = pd.melt(data, id_vars=["Instance"], var_name="Method", value_name="Value")
+
+    data['Method'] = pd.Categorical(data['Method'], categories=methods, ordered=False)
+    # Run LMER
+    model = smf.mixedlm("Value ~ Method", data=data, groups=data["Instance"])
+    result = model.fit()
+
+    log_data = copy.deepcopy(data)
+    log_data['Value'] = np.log(log_data['Value'])
+    log_model = smf.mixedlm(f"Value ~ Method", data=log_data, groups=data["Instance"])
+    log_result = log_model.fit()
+    # Obtain the residuals
+    _, p = stats.shapiro(result.resid)
+    _, p_log =  stats.shapiro(log_result.resid)
+    return p, p_log
+
+results = pd.DataFrame()
 for name in PATHS.keys():
-    lmem_normality(name)
+    results.loc[name,['raw','log']]= lmem_normality(name)
+results.to_csv('./Data/Results/normality_residuals.csv')
